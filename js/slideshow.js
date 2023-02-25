a960fb0c0f85dd4b74afbba1dd1708dc42dc23e5
Samuel Emeka <samuelemeka2021@outlook.com>
2023-02-25 15:23:04 +0100

    edited html and css files

--- js/slideshow.js
+++ js/slideshow.js
@@ -9,11 +9,22 @@ function plusDivs(n) {
 function currentDiv(n) {
     showDivs(slideIndex = n);
 }
+// Slideshow Apartment Images
+var slideIndex = 1;
+showDivs2(slideIndex);
+
+function plusDivs2(n) {
+    showDivs2(slideIndex += n);
+}
+
+function currentDiv2(n) {
+    showDivs2(slideIndex = n);
+}
 
 function showDivs(n) {
     var i;
-    var x = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
+    var x = document.getElementsByClassName("mySlides1");
+    var dots = document.getElementsByClassName("demo1");
     if (n > x.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = x.length }
     for (i = 0; i < x.length; i++) {
@@ -26,7 +37,7 @@ function showDivs(n) {
     dots[slideIndex - 1].className += " w3-opacity-off";
 }
 
-function showDivs(n) {
+function showDivs2(n) {
     var i;
     var x = document.getElementsByClassName("mySlides2");
     var dots = document.getElementsByClassName("demo2");
