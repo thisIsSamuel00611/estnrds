c2b2003fc47587b61360396ec13884c6f45a35cf
Samuel Emeka <samuelemeka2021@outlook.com>
2023-02-25 15:17:26 +0100

    edited html and css files

--- js/slideshow.js
+++ js/slideshow.js
@@ -25,3 +25,19 @@ function showDivs(n) {
     x[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " w3-opacity-off";
 }
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides2");
+    var dots = document.getElementsByClassName("demo2");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+    }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " w3-opacity-off";
+}
